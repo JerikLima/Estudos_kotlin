de00420544ff8aed82a97ef70291fcc6168cf21e
Jeriklima <jonherik123@gmail.com>
2024-01-10 15:14:49 -0300

    logica basica de uma conta bancaria feita

--- kotlin_shoppinglist/app/src/main/java/com/jeriklima/shoppinglist/BankAccount.kt
+++ kotlin_shoppinglist/app/src/main/java/com/jeriklima/shoppinglist/BankAccount.kt
@@ -1,4 +1,33 @@
 package com.jeriklima.shoppinglist
 
-class BankAccount {
+class BankAccount(
+    var accountHolder: String,
+    var balance: Double
+)
+{
+private val transactionHistory = mutableListOf<String>()
+    fun deposit(amount: Double){
+    balance += amount
+        transactionHistory.add("$accountHolder deposited $$amount")
+    }
+    fun withdraw(amount: Double){
+        if(amount <=balance){
+            balance -= amount
+            transactionHistory.add("$accountHolder withdraw $$amount")
+        }
+        else{
+            println("You don't have funds necessary to withdraw $$amount")
+        }
+    }
+
+    fun displayTransactionHistory(){
+    println("Transaction history for $accountHolder:")
+        for(transaction in transactionHistory){
+            println(transaction)
+        }
+
+
+    }
+
+
 }
--- kotlin_shoppinglist/app/src/main/java/com/jeriklima/shoppinglist/BankMain.kt
+++ kotlin_shoppinglist/app/src/main/java/com/jeriklima/shoppinglist/BankMain.kt
@@ -1,2 +1,11 @@
 package com.jeriklima.shoppinglist
 
+fun main() {
+
+    val deniseBankAccount = BankAccount("Denise Paola", 123.4)
+    println(deniseBankAccount.accountHolder)
+    deniseBankAccount.deposit(200.0)
+    deniseBankAccount.withdraw(1200.0)
+    deniseBankAccount.displayTransactionHistory()
+
+}
